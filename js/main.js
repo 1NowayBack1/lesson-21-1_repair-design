02ba63dd99e99375e4560e4b733a4d428d8664b6
No_way_Back <AkVaViTa1999@gmail.com>
2019-12-15 23:02:36 +0200

    feat: add 2 new jQuery scripts for button to top page, and script for modale icon

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -10,8 +11,29 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
   closeBtn.addEventListener('click', switchModal);
 });
+*/
 
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modal__Btn = $('[data-toggle=modal]'),
+    close__Btn = $('.modal__close');
+  
+  modal__Btn.on('click', function () {
+    modal.toggleClass('modal--visible');  
+  });
+  close__Btn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+});
 
-
-
-
+/* up button */
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    if ($('#upbutton').is(':hidden')) {
+      $('#upbutton').css({opacity : 1}).fadeIn('slow');
+    }
+  } else { $('#upbutton').stop(true, false).fadeOut('fast'); }
+});
+$('#upbutton').click(function() {
+  $('html, body').stop().animate({scrollTop : 0}, 300);
+});
